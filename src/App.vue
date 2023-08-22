fc3e86eb1657d2d38d2478405dc8bf8e2580367d
naridad <naridad+gitberk@protonmail.com>
2023-08-22 14:27:55 +0900

    remove add a shop, use database view

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/addashop">Add A Shop</RouterLink>
+        <RouterLink to="/db">Database</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -22,11 +22,6 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/addashop',
-      name: 'addashop',
-      component: () => import('../views/AddAShopView.vue')
-    },
-    {
       path: '/db',
       name: 'database',
       component: DbView
