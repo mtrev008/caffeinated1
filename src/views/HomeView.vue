88a22e466c3b524442a0342dc7e89f3ad81c416f
naridad <naridad+gitberk@protonmail.com>
2023-08-24 12:41:26 +0900

    fix missing imports/package lock

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 import Maps from '../components/Maps.vue';
-import Places from '../components/Places.vue';
 // import { Loader } from "@googlemaps/js-api-loader";
 import SearchBar from '../components/SearchBar.vue';
-import Maps2 from '../components/Maps2.vue';
 </script>
 
 <template>
